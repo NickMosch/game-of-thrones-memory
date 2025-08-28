fc5094af0ebd068fd4c50db009d40381df16d83f
NickMosch <inf2021144@ionio.gr>
2025-08-28 19:49:44 +0300

    Randomize function,score logic,reset logic

--- components/board.jsx
+++ components/board.jsx
@@ -2,22 +2,54 @@ import MemoryCard from "./card";
 import { data } from "../utils/data";
 import { useState } from "react";
 
-export default function Board(){
+export default function Board() {
+  const [cards, setCards] = useState(data);
+  const [bestScore, setBestScore] = useState(0);
 
-    const [cards,setCards] = useState(data);
+  const score = cards.filter((card) => card.clicked).length;
+  if (score > bestScore) {
+    setBestScore(score);
+  }
 
-    function handleCardClick(e){
-        const cardName = e.target.id?e.target.id:e.target.parentElement.id;
-        const cardsCopy = [...cards];
-        const indexOfCard = cardsCopy.findIndex(card => card.name == cardName);
-        if(cardsCopy[indexOfCard].clicked) return;
-        cardsCopy[indexOfCard] = {...cardsCopy[indexOfCard],clicked:true};
-        setCards(cardsCopy);
+  function shuffleCards(cardsCopy) {
+    for (let cardIndex = cards.length - 1; cardIndex > 0; cardIndex--) {
+      const cardToReplace = Math.floor(Math.random() * (cardIndex + 1));
+      [cardsCopy[cardToReplace], cardsCopy[cardIndex]] = [
+        cardsCopy[cardIndex],
+        cardsCopy[cardToReplace],
+      ];
     }
+    setCards(cardsCopy);
+  }
 
-    return (
-        <div>
-        {cards.map((card) => <MemoryCard key={card.name} name={card.name} url={card.url} onClick = {handleCardClick}></MemoryCard>)}
-        </div>
-    )
+    'https://api.giphy.com/v1/gifs/translate?api_key=nG3o74a4rIU0rqzuRFT7FClR9T1kSQj2&s=cats'
+
+  function handleCardClick(e) {
+    const cardName = e.target.id ? e.target.id : e.target.parentElement.id;
+    const cardsCopy = [...cards];
+    const indexOfCard = cardsCopy.findIndex((card) => card.name == cardName);
+    if (cardsCopy[indexOfCard].clicked) {
+      for (let cardIndex = 0; cardIndex < cardsCopy.length; cardIndex++) {
+        cardsCopy[cardIndex] = { ...cardsCopy[cardIndex], clicked: false };
+      }
+    } else {
+      cardsCopy[indexOfCard] = { ...cardsCopy[indexOfCard], clicked: true };
+    }
+    shuffleCards(cardsCopy);
+  }
+
+  return (
+    <div>
+      <p>Score is {score}</p>
+      <p>Best is {bestScore}</p>
+      {cards.map((card) => (
+        <MemoryCard
+          key={card.name}
+          name={card.name}
+          url={card.url}
+          onClick={handleCardClick}
+        ></MemoryCard>
+      ))}
+    </div>
+  );
 }
--- utils/data.js
+++ utils/data.js
@@ -2,6 +2,9 @@ const data = [
   { name: "Archer", clicked: false, url: "../src/assets/archers.jpeg" },
   { name: "Barbarian", clicked: false, url: "../src/assets/barbarian.jpg" },
   { name: "Goblin", clicked: false, url: "../src/assets/goblin.jpg" },
+  { name: "Giant", clicked: false, url: "../src/assets/goblin.jpg" },
+  { name: "Knight", clicked: false, url: "../src/assets/goblin.jpg" },
+  { name: "Bomber", clicked: false, url: "../src/assets/goblin.jpg" },
 ];
 
 export { data };
